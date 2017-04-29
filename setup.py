92dd1ab0cd5915ac85f123481ad098c2f816cf5f
Xiaoli Wu <xrayxiaoli@gmail.com>
2017-04-29 11:07:36 +0800

    bump() v0.0.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ dependencies = ['click']
 
 setup(
     name='wegene-weapp-cli',
-    version='0.0.2',
+    version='0.0.3',
     url='https://github.com/wegene-llc/wegene-weapp-cli',
     license='MIT',
     author='Xiaoli Wu',
