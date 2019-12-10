86f73119ac4e7efdf3c28273f5594e71abb34cb2
Xiaoli Wu <wxl@wegene.com>
2019-12-10 17:35:36 +0800

    fix(): Util 中变量名错误; add(): 新增 WeGene 格式判断 util; bump(): 1.0.4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ dependencies = ['click', 'wget', 'markdown']
 
 setup(
     name='wegene-weapp-cli',
-    version='1.0.3',
+    version='1.0.4',
     url='https://github.com/wegene-llc/wegene-weapp-cli',
     license='MIT',
     author='Xiaoli Wu',
